16e027c34926b4a26eb44eb78c72418fb755838d
alexey-va <freedeeml@gmail.com>
2026-09-03 22:22:28 +0300

    Use Arc Core 2.4.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ru.ruscrafting"
-version = "1.0.16"
+version = "1.0.17"
 description = "Survival-friendly builder tools for RusCrafting"
 
 val integrationTestSourceSet = sourceSets.create("integrationTest") {
@@ -31,13 +31,13 @@ kotlin { jvmToolchain(25) }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("ru.ruscrafting.arc:arc-core:2.4.4")
-    implementation("ru.ruscrafting.arc:arc-core-logging:2.4.4")
-    implementation("ru.ruscrafting.arc:arc-core-menu:2.4.4")
-    implementation("ru.ruscrafting.arc:arc-core-paper:2.4.4")
-    implementation("ru.ruscrafting.arc:arc-core-paper-menu:2.4.4")
+    implementation("ru.ruscrafting.arc:arc-core:2.4.5")
+    implementation("ru.ruscrafting.arc:arc-core-logging:2.4.5")
+    implementation("ru.ruscrafting.arc:arc-core-menu:2.4.5")
+    implementation("ru.ruscrafting.arc:arc-core-paper:2.4.5")
+    implementation("ru.ruscrafting.arc:arc-core-paper-menu:2.4.5")
     implementation("org.snakeyaml:snakeyaml-engine:3.0.1")
-    implementation("ru.ruscrafting.arc:arc-core-sql:2.4.4")
+    implementation("ru.ruscrafting.arc:arc-core-sql:2.4.5")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("com.jeff-media:custom-block-data:2.2.4")
     implementation("de.tr7zw:item-nbt-api:2.15.7")
@@ -60,7 +60,7 @@ dependencies {
     testImplementation("io.mockk:mockk:1.14.7")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.3")
-    testImplementation("ru.ruscrafting.arc:arc-core-paper-testing:2.4.4")
+    testImplementation("ru.ruscrafting.arc:arc-core-paper-testing:2.4.5")
     testImplementation("com.sk89q.worldedit:worldedit-bukkit:7.3.18")
     testImplementation("com.github.MilkBowl:VaultAPI:1.7") {
         exclude(group = "org.bukkit", module = "bukkit")
@@ -69,7 +69,7 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.3")
 
     "integrationTestImplementation"(sourceSets.test.get().output)
-    "integrationTestImplementation"("ru.ruscrafting.arc:arc-core-integration-testing:2.4.4")
+    "integrationTestImplementation"("ru.ruscrafting.arc:arc-core-integration-testing:2.4.5")
     configurations["integrationTestImplementation"].extendsFrom(configurations["testImplementation"])
     configurations["integrationTestRuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
 }
