c84d2428f5d5b71bc3b7f88cbc62a69542f91382
alexey-va <freedeeml@gmail.com>
2026-09-25 21:05:06 +0300

    build: update arc-core to 2.7.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,14 +32,14 @@ kotlin { jvmToolchain(25) }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("ru.ruscrafting.arc:arc-core:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-logging:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-menu:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-paper:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-paper-menu:2.7.11")
+    implementation("ru.ruscrafting.arc:arc-core:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-logging:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-menu:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-paper:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-paper-menu:2.7.13")
     implementation("org.snakeyaml:snakeyaml-engine:3.0.1")
-    implementation("ru.ruscrafting.arc:arc-core-sql:2.7.11")
-    compileOnly("ru.ruscrafting.arc:arc-core-paper-api:2.7.11")
+    implementation("ru.ruscrafting.arc:arc-core-sql:2.7.13")
+    compileOnly("ru.ruscrafting.arc:arc-core-paper-api:2.7.13")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("com.jeff-media:custom-block-data:2.2.4")
     implementation("de.tr7zw:item-nbt-api:2.15.7")
@@ -64,8 +64,8 @@ dependencies {
     testImplementation("com.github.retrooper:packetevents-spigot:2.12.1")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.3")
-    testImplementation("ru.ruscrafting.arc:arc-core-paper-testing:2.7.11")
-    testImplementation("ru.ruscrafting.arc:arc-core-paper-api:2.7.11")
+    testImplementation("ru.ruscrafting.arc:arc-core-paper-testing:2.7.13")
+    testImplementation("ru.ruscrafting.arc:arc-core-paper-api:2.7.13")
     testImplementation("com.sk89q.worldedit:worldedit-bukkit:7.3.18")
     testImplementation("com.github.Slimefun:Slimefun4:4.10")
     testImplementation("com.github.MilkBowl:VaultAPI:1.7") {
@@ -75,7 +75,7 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.3")
 
     "integrationTestImplementation"(sourceSets.test.get().output)
-    "integrationTestImplementation"("ru.ruscrafting.arc:arc-core-integration-testing:2.7.11")
+    "integrationTestImplementation"("ru.ruscrafting.arc:arc-core-integration-testing:2.7.13")
     configurations["integrationTestImplementation"].extendsFrom(configurations["testImplementation"])
     configurations["integrationTestRuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
 }
